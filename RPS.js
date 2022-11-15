355052767c55380b7a52d046d09944151cf5493b
Marshall B <110133622+SchoolyB@users.noreply.github.com>
2022-11-14 21:17:11 -0600

    Refractor/add TODOs in js file

--- RPS.js
+++ RPS.js
@@ -1,64 +1,99 @@
-// TODO: need to find bug that causes conditionals to run even when they shouldn't
+// TODO: FIGURE OUT HOW TO KILL PROGRAM WHEN USER ENTERS NO
+// TODO: CONTINUE TO BUILD ON FUNCTION SO USER CAN SEE A SCORE
+// TODO: BUILD GUI IN HTML/CSS
 
-//  MAIN FUNCTION
-function main() {
+let userScore = 0  
+let compScore = 0
 
-  // DECLARING COMPINPUT VAR TO USE MATH.RANDOM()
-    let compInput = Math.random();
-    
-  // DECLARING POSSIBLE OPTION FOR USER AND COMP
-    let option = [
-      "rock",
-      "paper",
-      "scissors"
-    ];
-  
-    // INITIAL PROMPT AND DECLARING USERINPUT VAR
-    let userInput = prompt(`Please select either,
-    ${option[0]},
-    ${option[1]},
-    ${option[2]}`);
-    //  CALL BACK TO USER CHOICE
-  alert(`You chose ${userInput}`) 
-  
-    // EQUATING COMP MATH DECISIONS TO GIVEN OPTION
-    if (compInput <= .33) {
-      compInput = option[0]
-    } else if (compInput >= .66) {
-      compInput = option[1]
-    } else { 
-      compInput = option[2]
-    }
- 
-  
-    
-  // USER ROCK CONDITIONS
-    if (userInput && compInput === option[0]) {
-      alert(`Both players chose ${option[0]} its a draw`)
-    } else if (userInput === option[0] && compInput === option[1]){
-      alert(`You chose  ${option[0]}, and the computer chose ${option[1]}...Paper covers rock you lose`)
-    } else if (userInput === option[0] && compInput === option[2]) {
-      alert(`You chose ${option[0]} and the computer chose ${option[2]}...Rock smashes scissors you win`)
-    } 
-  
-    // USER PAPER CONDITIONS
-    if (userInput && compInput === option[1]) {
-      alert(`Both players chose ${option[1]} its a draw`)
-    } else if (userInput === option[1] && compInput === option[0]){
-      alert(`You chose  ${option[1]}, and the computer chose ${option[0]}...Paper covers rock you win`)
-    } else if (userInput === option[1] && compInput === option[2]) {
-      alert(`You chose ${option[1]} and the computer chose ${option[2]}...Scissors cuts paper you lose`)
-    } 
-  
-    // USER SCISSORS CONDITIONS
-    if (userInput && compInput === option[2]) {
-      alert(`Both players chose ${option[2]} its a draw`)
-    } else if (userInput === option[2] && compInput === option[0]){
-      alert(`You chose  ${option[2]}, and the computer chose ${option[0]}...Rock smashes scissors you lose`)
-    } else if (userInput === option[2] && compInput === option[1]) {
-      alert(`You chose ${option[2]} and the computer chose ${option[1]}...Scissors cuts paper you win`)
-    } 
-  
+
+function play_again() {
+  RPS();
+}
+
+
+function tell_score() {
+alert(`Your score is ${userScore} to the computers ${compScore}`)
+}
+function kill() {
+  alert("ENDING PROGRAM")
+
+  close();
+}
+
+function play_again_prompt() {
+  let user_play_again = prompt("would you like to play again? enter yes or no")
+  if (user_play_again === "yes" || "Y" || "y" || "YES" || "yea" || "yea" || "yep" || "yessir") {
+    RPS();
+  } else if (play_again === "no" || "N" || "n" || "nope" || "NO" || "nah") {
+    score();
+    close();
   };
+
+}
+
+
+  function RPS() {
+    let compInput = Math.random()
+    let userInput = prompt("Please choose either rock paper or scissors")
+
+    if (userInput === "rock") {
+      userInput = "rock"
+    } else if (userInput === "paper") {
+      userInput = "paper"
+    } else if (userInput === "scissors") {
+      userInput = "scissors"
+    }
+
+    if (compInput <= .33) {
+      compInput = "rock"
+    } else if (compInput >= .66) {
+      compInput = "paper"
+    } else {
+      compInput = "scissors"
+    }
+
+    if (userInput === compInput) {
+      alert("its a draw")
+    };
+
+
+    // USER ROCK INPUT
+    if (userInput === "rock" && compInput === "paper") {
+      alert(`You chose ${userInput} and the computer chose ${compInput}...paper covers rock you lose`)
+      compScore + 1
+    }
   
-  main();
+    if (userInput === "rock" && compInput === "scissors") {
+      alert(`You  chose ${userInput} and the computer chose ${compInput}...rock smashes scissors you win!`)
+      userScore + 1
+    }
+
+
+    //USER PAPER INPUT  
+    if (userInput === "paper" && compInput === "rock") {
+      alert(`You  chose ${userInput} and the computer chose ${compInput}...paper covers rock you win`)
+      userScore + 1
+    }
+
+    if (userInput === "paper" && compInput === "scissors") {
+      alert(`You chose ${userInput} and the computer chose ${compInput}...paper covers rock you lose`)
+      compScore + 1
+    }
+  
+  
+    //USER SCISSORS INPUT
+    if (userInput === "scissors" && compInput === "rock") {
+      alert(`You chose ${userInput} and the computer chose ${compInput}...rock smashes scissors you lose`)
+      compScore + 1
+    }
+  
+    if (userInput === "scissors" && compInput === "paper") {
+      alert(`You chose ${userInput} and the computer chose ${compInput}...scissors cuts paper you win`)
+      userScore + 1
+    }
+  
+    play_again_prompt();
+
+}
+
+RPS();
