5668e03aed6c2c23501631c6e8b6954b1e39b96c
Schooly <marshallbcodes@gmail.com>
2022-11-23 10:37:33 -0600

    Clean up function syntax

--- backend/modules.js
+++ backend/modules.js
@@ -1,28 +1,28 @@
 // TODO: NEED TO REDO MODULE ONE 
 // START IF MODULE ONE...RPS BUTTON FUNCTIONALITY
-function rock_button_clicked() {
-  userInput = "rock";
+rock_button_clicked = () => {
+  userInput = "rock"
 }
 
-function paper_button_clicked(){
-  userInput = "paper";
+paper_button_clicked = () => {
+  userInput = "paper"
 }
 
-function scissors_button_clicked(){
+scissors_button_clicked = () => {
   userInput = "scissors"
 }
 
 document.getElementById("rock_btn").addEventListener("click",rock_button_clicked);
 document.getElementById("paper_btn").addEventListener("click", paper_button_clicked);
 document.getElementById("scissors_btn").addEventListener("click", scissors_button_clicked);
+
 // END OF MODULE ONE
 
 
 // START OF MODULE TWO...SELECTION AREA HIDE IMAGES USING JS RATHER THAN CSS
 // TODO: need to rename vars
-
-function hide_images_on_load() {
-  //hides rock
+hide_images_on_load = () => {
+  
   const a = document.getElementById("rock_hand");
   a.style.visibility = "hidden";
 
@@ -57,27 +57,30 @@ function hide_images_on_load() {
 
 
 //START OF MODULE THREE...SHOW SELECT AREA ROCK PAPER SCISSORS AND SHOOT IMAGES ON A TIMER 
-function makeRockVisible() {
+makeRockVisible = () => {
   const show_rock = document.getElementById("rock_hand")
   show_rock.style.visibility = "visible";
-  }
+}
 
-function makePaperVisible() {
+makePaperVisible = () => {
   const show_rock = document.getElementById("paper_hand")
   show_rock.style.visibility = "visible";
-  }
+  
+}
 
-function makeScissorsVisible() {
+makeScissorsVisible = () => {
   const show_rock = document.getElementById("scissors_hand")
   show_rock.style.visibility = "visible";
-  }
+  
+}
 
-function makeShootVisible() {  
+makeShootVisible = () => {
   const show_shoot = document.getElementById("shoot")
   show_shoot.style.visibility = "visible";
-  }
+  
+}
 
-function show_rps_on_timer() { //shows images on a timer when called FINALLY GOT IT TO WORK
+show_rps_on_timer = () => {
   show_hand_container()
   setTimeout(makeRockVisible, 1000)
   setTimeout(makePaperVisible, 2000)
@@ -86,13 +89,13 @@ function show_rps_on_timer() { //shows images on a timer when called FINALLY GOT
   setTimeout(hide_images_after_shown, 5000)
   setTimeout(hide_hand_container, 5000)
 }
+
 // END OF MODULE THREE
 
 
 // START OF MODULE FOUR...HIDING RPS IMAGES AFTER THEY HAVE APPEARED ON SCREEN// KEEP SHOOT ON SCREEN UNTIL USER AND COMPUTER CHOOSE
 // TODO:change var names
-function hide_images_after_shown() {
-
+hide_images_after_shown = () => {
   const x = document.getElementById("rock_hand")
   x.style.visibility = "hidden"
 
@@ -108,12 +111,12 @@ function hide_images_after_shown() {
 
 
 // START OF MODULE FIVE...SHOWS  AND HIDES THE CONTAINER THAT HAS THE RPS HAND IMAGES 
-function show_hand_container() {
+show_hand_container = () => { 
   const showHandContainer = document.getElementById("hand_container")
   showHandContainer.style.visibility = "visible"
 }
 
-function hide_hand_container() {
+hide_hand_container = () => { 
   const hideHandContainer = document.getElementById("hand_container")
   hideHandContainer.style.visibility = "hidden"
 }
@@ -121,7 +124,7 @@ function hide_hand_container() {
 
 
 // START OF MODULE SIX...HIDES THE SHOOT IMAGE AFTER THE BOTH USERS MAKE THEIR DECISION
-// TODO: incomplete function
+// TODO: incomplete function need to finish
 function hide_shoot_after_shown() {
   const hideShoot = document.getElementById("shoot")
   hideShoot.style.visibility = "hidden"
